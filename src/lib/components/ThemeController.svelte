74b6d2f15e5b2f8e1fede0783e519be7b6e65807
YvesCheung88 <yves.cheung.88@gmail.com>
2024-09-12 11:39:41 +0200

    theme controller implemented

---
+++ src/lib/components/ThemeController.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import Moon from '$lib/images/Moon.svelte';
+	import Sun from '$lib/images/Sun.svelte';
+
+	let theme = localStorage.getItem('theme') ?? 'dark';
+	$: if (browser && document) {
+		document.documentElement.dataset.theme = theme;
+		localStorage.setItem('theme', theme);
+	}
+
+	function toggle() {
+		if (theme === 'dark') {
+			theme = 'light';
+		} else {
+			theme = 'dark';
+		}
+	}
+</script>
+
+<button on:click={toggle}>
+	{#if theme === 'dark'}
+		<Moon />
+	{:else}
+		<Sun />
+	{/if}
+</button>
