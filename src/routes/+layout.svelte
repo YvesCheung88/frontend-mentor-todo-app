d0c5db281b035511cc84090a4af272a4227ef0c9
YvesCheung88 <yves.cheung.88@gmail.com>
2024-09-18 16:43:42 +0200

    setup layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,5 @@
 <script>
 	import '../app.css';
-	import ThemeController from '$lib/components/ThemeController.svelte';
-	import { fade } from 'svelte/transition';
-	import { theme } from '$lib/themeStore';
 </script>
 
-<main>
-	{#key $theme}
-		<img
-			transition:fade
-			src={$theme === 'dark' ? '/images/bg-desktop-dark.jpg' : '/images/bg-desktop-light.jpg'}
-			alt="background"
-		/>
-	{/key}
-
-	<ThemeController />
-	<slot />
-</main>
-
-<style>
-	main {
-		position: relative;
-		padding-top: 256px;
-		& img {
-			position: absolute;
-			height: 256px;
-			top: 0px;
-		}
-	}
-</style>
+<slot />
