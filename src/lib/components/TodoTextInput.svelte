c6ed89d9f4aa5e787aa47142e723f8d8802735bf
YvesCheung88 <yves.cheung.88@gmail.com>
2024-09-19 14:51:25 +0200

    TodoTextInput implemented

---
+++ src/lib/components/TodoTextInput.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { createTodo, type Todo } from '$lib/stores/Todo';
+	export let todoList: Todo[];
+
+	let todoTextValue = '';
+
+	function onSubmitToList() {
+		if (todoTextValue.trim() !== '') {
+			const newTodo = createTodo({
+				text: todoTextValue
+			});
+			todoList = [...todoList, newTodo];
+			todoTextValue = '';
+		}
+	}
+</script>
+
+<form on:submit|preventDefault={onSubmitToList}>
+	<input type="text" bind:value={todoTextValue} placeholder="Create a new todo..." />
+</form>
+
+<style>
+	form {
+		display: grid;
+		background-color: grey;
+		padding: 20px;
+		& input {
+			border-radius: 4px;
+			padding: 10px;
+		}
+	}
+</style>
